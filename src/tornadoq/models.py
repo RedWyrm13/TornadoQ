eaa3b284231d5f40c3c50c5c300ead269791ec47
Andrew Maciejunes <Andrewmaciejunes@yahoo.com>
2026-01-04 14:25:17 -0500

    Made input_size a required parameter for InitializeModel

--- src/tornadoq/models.py
+++ src/tornadoq/models.py
@@ -2,7 +2,7 @@ from tornadoq.pqc import InitializePQC
 import torch.nn as nn
 import torch
 
-def InitializeModel(model, load_path = None, classifier = "binary", input_size = 8):
+def InitializeModel(model, input_size, load_path = None, classifier = "binary"):
 
     ############ Conditionals for all model types ############## 
     ### DNN Various Input ###
